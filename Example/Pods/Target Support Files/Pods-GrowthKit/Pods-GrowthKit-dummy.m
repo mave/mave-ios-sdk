63d0ea4581e3bd861e1b8b553d95e8da00889edd
Danny Cosson <dcosson@gmail.com>
2014-10-09 22:32:05 -0400

    remove autogenerated example stuff

--- Example/Pods/Target Support Files/Pods-GrowthKit/Pods-GrowthKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GrowthKit : NSObject
-@end
-@implementation PodsDummy_Pods_GrowthKit
-@end
--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  GrowthKitTests.m
-//  GrowthKitTests
-//
-//  Created by Danny Cosson on 10/09/2014.
-//  Copyright (c) 2014 Danny Cosson. All rights reserved.
-//
-
-${TEST_EXAMPLE}
