3b2943289bcddfd346fdb17aa3ad979f6ac98d8d
Danny Cosson <dcosson@gmail.com>
2015-01-18 15:26:12 -0500

    Add a custom search bar

---
+++ MaveSDK/Views/MAVESearchBar.h
@@ -0,0 +1,13 @@
+//
+//  MAVESearchBar.h
+//  MaveSDK
+//
+//  Created by Danny Cosson on 1/16/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAVESearchBar : UITextField
+
+@end
---
+++ MaveSDK/Views/MAVESearchBar.m
@@ -0,0 +1,67 @@
+//
+//  MAVESearchBar.m
+//  MaveSDK
+//
+//  Created by Danny Cosson on 1/16/15.
+//
+//
+
+#import "MAVESearchBar.h"
+
+CGFloat const MAVESearchBarRowHeight = 44.0;
+
+@implementation MAVESearchBar
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.backgroundColor = [UIColor whiteColor];
+        self.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+        self.font = [self placeholderFont];
+        self.attributedPlaceholder = [[NSAttributedString alloc]
+            initWithString:@"Enter name or phone number"
+            attributes:@{NSForegroundColorAttributeName: [self placeholderTextColor],
+                         NSFontAttributeName: [self placeholderFont],
+        }];
+
+        [self setupLeftLabelView];
+    }
+    return self;
+}
+
+// UI colors & sizes to use, will be displayOptions later
+- (UIColor *)placeholderTextColor {
+    return [UIColor grayColor];
+}
+
+- (UIFont *)placeholderFont {
+    return [UIFont systemFontOfSize:16];
+}
+
+- (void)setupLeftLabelView {
+    CGFloat paddingXPre = 8;
+    CGFloat paddingXPost = 10;
+
+    UILabel *label = [[UILabel alloc] init];
+    label.text = @"To:";
+    label.textColor = [self placeholderTextColor];
+    label.font = [self placeholderFont];
+
+    CGSize textSize = [label.text sizeWithAttributes:@{NSFontAttributeName:label.font}];
+    textSize.width = ceil(textSize.width);
+    textSize.height = ceil(textSize.height);
+
+
+    UIView *leftView = [[UIView alloc] init];
+    [leftView addSubview:label];
+
+    label.frame = CGRectMake(paddingXPre, 0, textSize.width, textSize.height);
+    leftView.frame = CGRectMake(0,
+                                0,
+                                paddingXPre + textSize.width + paddingXPost,
+                                textSize.height);
+
+    self.leftViewMode = UITextFieldViewModeAlways;
+    self.leftView = leftView;
+}
+
+@end
