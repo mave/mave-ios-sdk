7a08b2caef1db735272b95ea9c55a0e47e52857c
Danny Cosson <dcosson@gmail.com>
2015-01-25 13:31:39 -0500

    add new files for merkle tree

---
+++ MaveSDK/Utils/MAVEMerkleTree/MAVEMerkleTreeNode.h
@@ -0,0 +1,13 @@
+//
+//  MAVEMerkleTreeNode.h
+//  MaveSDK
+//
+//  Created by Danny Cosson on 1/25/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MAVEMerkleTreeNode : NSObject
+
+@end
---
+++ MaveSDK/Utils/MAVEMerkleTree/MAVEMerkleTreeNode.m
@@ -0,0 +1,13 @@
+//
+//  MAVEMerkleTreeNode.m
+//  MaveSDK
+//
+//  Created by Danny Cosson on 1/25/15.
+//
+//
+
+#import "MAVEMerkleTreeNode.h"
+
+@implementation MAVEMerkleTreeNode
+
+@end
