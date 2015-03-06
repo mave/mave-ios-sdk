f11a1d32383bdf6766cefc26f4726403059a53a7
Danny Cosson <dcosson@gmail.com>
2015-03-06 13:57:34 -0500

    Add a MAVESharer class, follows the pattern used by the permission prompt handler that converts a method + delegate for response into a method with a response block, using a trick where it retains itself until the response block finishes

---
+++ MaveSDK/Controllers/MAVESharer.h
@@ -0,0 +1,13 @@
+//
+//  MAVESharer.h
+//  MaveSDK
+//
+//  Created by Danny Cosson on 3/6/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MAVESharer : NSObject
+
+@end
---
+++ MaveSDK/Controllers/MAVESharer.m
@@ -0,0 +1,13 @@
+//
+//  MAVESharer.m
+//  MaveSDK
+//
+//  Created by Danny Cosson on 3/6/15.
+//
+//
+
+#import "MAVESharer.h"
+
+@implementation MAVESharer
+
+@end
