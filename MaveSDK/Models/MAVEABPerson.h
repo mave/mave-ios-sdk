629af13f5fe6da6fe651d681ff003344dcaa7c04
Danny Cosson <dcosson@gmail.com>
2015-03-13 16:44:33 -0400

    replace all comment copyrights Growthkit -> Mave Technologies. It also removed trailing newlines from all files so theres a lot of noise in the diff

--- MaveSDK/Models/MAVEABPerson.h
+++ MaveSDK/Models/MAVEABPerson.h
@@ -3,7 +3,7 @@
 //  MaveSDKDevApp
 //
 //  Created by dannycosson on 9/25/14.
-//  Copyright (c) 2014 Growthkit Inc. All rights reserved.
+//  Copyright (c) 2015 Mave Technologies, Inc. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- MaveSDK/Views/MAVEInviteMessageView.h
+++ MaveSDK/Views/MAVEInviteMessageView.h
@@ -3,7 +3,7 @@
 //  MaveSDKDevApp
 //
 //  Created by dannycosson on 10/2/14.
-//  Copyright (c) 2014 Growthkit Inc. All rights reserved.
+//  Copyright (c) 2015 Mave Technologies, Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
