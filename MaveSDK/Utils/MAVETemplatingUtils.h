95e73a8933aa1a60d4fc92bd034fa5dd969b7583
Danny Cosson <dcosson@gmail.com>
2015-03-25 10:17:04 -0400

    handle non-string types in customData dictionaries

--- MaveSDK/Utils/MAVETemplatingUtils.h
+++ MaveSDK/Utils/MAVETemplatingUtils.h
@@ -11,6 +11,10 @@
 
 @interface MAVETemplatingUtils : NSObject
 
+// Convert any id to a string value if possible, or return nil if not possible.
+// Will convert NSNumber and NSNull successfully, not arbitrary objects.
++ (NSString *)convertValueToString:(id)value;
+
 // Helper method to interpolate the template string using the current context.
 // Available fields in template are user.* and customData.*
 + (NSString *)interpolateTemplateString:(NSString *)templateString
--- MaveSDK/Utils/MAVETemplatingUtils.m
+++ MaveSDK/Utils/MAVETemplatingUtils.m
@@ -15,20 +15,41 @@
 + (NSString *)interpolateTemplateString:(NSString *)templateString withUser:(MAVEUserData *)user customData:(NSDictionary *)customData {
 
     NSMutableDictionary *interpolationDict = [[NSMutableDictionary alloc] init];
+
+    // All these user fields are NSStrings
     [interpolationDict setValue:user.userID forKey:@"user.userID"];
     [interpolationDict setValue:user.firstName forKey:@"user.firstName"];
     [interpolationDict setValue:user.lastName forKey:@"user.lastName"];
     [interpolationDict setValue:user.fullName forKey:@"user.fullName"];
 
-    NSString *namespacedKey, *key, *value;
+    NSString *namespacedKey, *key, *stringValue;
     for (key in customData) {
-        value = [customData valueForKey:key];
+        if (![key isKindOfClass:[NSString class]]) {
+            continue;
+        }
+        stringValue = [self convertValueToString:[customData objectForKey:key]];
+        if (!stringValue) {
+            continue;
+        }
         namespacedKey = [@"customData." stringByAppendingString:key];
-        [interpolationDict setValue:value forKey:namespacedKey];
+        [interpolationDict setValue:stringValue forKey:namespacedKey];
     }
 
     NSString *output = [templateString templateFromDict:[NSDictionary dictionaryWithDictionary:interpolationDict]];
     return output;
+}
+
++ (NSString *)convertValueToString:(id)value {
+    if ([value isKindOfClass:[NSString class]]) {
+        return value;
+    }
+    if ([value isKindOfClass:[NSNumber class]]) {
+        return [((NSNumber *)value) stringValue];
+    }
+    if (value == (id)[NSNull null]) {
+        return @"<null>";
+    }
+    return nil;
 }
 
 + (NSString *)interpolateWithSingletonDataTemplateString:(NSString *)templateString {
