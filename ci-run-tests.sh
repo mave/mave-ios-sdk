b6ad23ecbcc5cbe3b5db515918423d66c06f6a42
Danny Cosson <dcosson@gmail.com>
2015-03-31 18:12:28 -0400

    add a ci script that we can use for a matrix build w/ ios version and iphone version, and add a simple travis.yml file

---
+++ ci-run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "${DESTINATION_OS}" ] ; then DESTINATION_OS="8.2" ; fi
+if [ -z "${DESTINATION_NAME}"] ; then DESTINATION_NAME="iPhone 6" ; fi
+
+DESTINATION="platform=iOS Simulator,OS=${DESTINATION_OS},name=${DESTINATION_NAME}"
+
+xctool \
+    -workspace MaveSDK.xcworkspace \
+    -scheme DemoApp \
+    -configuration UnitTesting \
+    -sdk iphonesimulator \
+    -destination "${DESTINATION}" \
+    test \
+    -freshInstall \
+    -freshSimulator
