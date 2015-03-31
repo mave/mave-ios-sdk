2244ff3c4c4193b2e6f2ff1c4ed751af9d3455e9
Danny Cosson <dcosson@gmail.com>
2015-03-31 12:16:10 -0400

    Add a new navigation controller to allow setting the status bar color when displaying the invite page modally

---
+++ MaveSDK/Controllers/MAVEWrapperNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MAVEWrapperNavigationController.h
+//  MaveSDK
+//
+//  Created by Danny Cosson on 3/31/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAVEWrapperNavigationController : UINavigationController
+
+@end
