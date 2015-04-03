e6147f5e73a726fdc75ad8647a178cc5fe31fd56
Danny Cosson <dcosson@gmail.com>
2015-04-03 09:20:35 -0400

    split up build & run steps, add retries since running is fast (~10 sec) but pod installing & building is slow (~3min on travis)

--- ci-run-tests.sh
+++ ci-run-tests.sh
@@ -5,11 +5,33 @@ if [ -z "${DESTINATION_NAME}"] ; then DESTINATION_NAME="iPhone 6" ; fi
 
 DESTINATION="platform=iOS Simulator,OS=${DESTINATION_OS},name=${DESTINATION_NAME}"
 
-xctool \
-    -workspace MaveSDK.xcworkspace \
-    -scheme DemoApp \
-    -configuration UnitTesting \
-    -sdk iphonesimulator \
-    -destination "${DESTINATION}" \
-    test \
-    -freshSimulator
+clean_and_build_tests() {
+    xctool clean
+    xctool \
+        -workspace MaveSDK.xcworkspace \
+        -scheme DemoApp \
+        -configuration UnitTesting \
+        -sdk iphonesimulator \
+        -destination "${DESTINATION}" \
+        build-tests
+}
+
+run_tests() {
+    xctool run-tests -resetSimulator
+}
+
+clean_and_build_tests
+RETURN_VAL=$?
+[ $RETURN_VAL -ne 0 ] && echo "\n\n\n** EXITING BECAUSE BUILD-TESTS FAILED **\n\n" &&  exit 1
+
+run_tests
+RETURN_VAL=$?
+[ $RETURN_VAL -eq 0 ] && exit 0
+
+printf "\n\n\n** RUN-TESTS RETRYING 1 **\n\n"
+run_tests
+RETURN_VAL=$?
+[ $RETURN_VAL -eq 0 ] && exit 0
+
+printf "\n\n\n** RUN-TESTS RETRYING 2 **\n\n"
+run_tests
