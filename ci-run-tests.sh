931c4ca7430c4e8889addddcd2ed31e48d2995c8
Danny Cosson <dcosson@gmail.com>
2015-04-03 09:45:32 -0400

    typo in checking env vars in ci script

--- ci-run-tests.sh
+++ ci-run-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ -z "${DESTINATION_OS}" ] ; then DESTINATION_OS="8.2" ; fi
-if [ -z "${DESTINATION_NAME}"] ; then DESTINATION_NAME="iPhone 6" ; fi
+if [ -z "${DESTINATION_NAME}" ] ; then DESTINATION_NAME="iPhone 6" ; fi
 
 DESTINATION="platform=iOS Simulator,OS=${DESTINATION_OS},name=${DESTINATION_NAME}"
+
+echo "Running with destination ${DESTINATION}"
 
 clean_and_build_tests() {
     xctool clean
