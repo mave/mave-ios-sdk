0e8cc4ed060905b772eac6cbfea27a8df6d79bf4
Danny Cosson <dcosson@gmail.com>
2015-06-03 12:24:33 -0400

    when leaving the search view, scroll to same row in the regular view. If someone is selected in the search view scroll to that person, otherwise the top person in the search results

--- MaveSDK/Controllers/MAVEContactsInvitePageSearchManager.m
+++ MaveSDK/Controllers/MAVEContactsInvitePageSearchManager.m
@@ -25,8 +25,24 @@
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     NSString *newText = [textField.text stringByReplacingCharactersInRange:range withString:string];
     if ([newText length] > 0 && [[newText substringFromIndex:[newText length]-1] isEqualToString:@"\n"]) {
+        MAVEABPerson *topResult = nil;
+        if ([self.dataManager.searchTableData count] > 0) {
+            for (MAVEABPerson *_person in self.dataManager.searchTableData) {
+                if (_person.selected) {
+                    topResult = _person;
+                    break;
+                }
+            }
+            if (!topResult) {
+                topResult = [self.dataManager.searchTableData objectAtIndex:0];
+            }
+        }
         [textField endEditing:YES];
         [self clearCurrentSearchInTextField:textField];
+        if (topResult) {
+            NSIndexPath *path = [self.dataManager indexPathOfFirstOccuranceInMainTableOfPerson:topResult];
+            [self.mainTable scrollToRowAtIndexPath:path atScrollPosition:UITableViewScrollPositionTop animated:NO];
+        }
         return NO;
     }
     if ([newText length] > 0) {
