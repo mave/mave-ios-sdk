7bf391ca082a912f028cbbc6367c9ca6ba07c90d
Danny Cosson <dcosson@gmail.com>
2015-07-07 14:36:49 -0400

    remove the change of default for testing

--- MaveSDK/Models/RemoteConfiguration/MAVERemoteConfigurationInvitePageChoice.m
+++ MaveSDK/Models/RemoteConfiguration/MAVERemoteConfigurationInvitePageChoice.m
@@ -35,7 +35,7 @@ NSString * const MAVERemoteConfigKeyInvitePageFallback = @"fallback_page";
 
 + (MAVEInvitePageType)invitePageTypeFromJSONStringName:(NSString *)pageType {
     if ([pageType isEqualToString:@"contacts_invite_page"]) {
-        return MAVEInvitePageTypeContactsInvitePageV3;
+        return MAVEInvitePageTypeContactsInvitePage;
     } else if ([pageType isEqualToString:@"contacts_invite_page_v2"]) {
         return MAVEInvitePageTypeContactsInvitePageV2;
     } else if ([pageType isEqualToString:@"contacts_invite_page_v3"]) {
