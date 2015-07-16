7fab35558af78016bd30fc756d84722159c164aa
Danny Cosson <dcosson@gmail.com>
2015-07-16 16:53:40 -0400

    we fixed this at a lower level, so we can delete it here

--- MaveSDK/Controllers/MAVESharer.m
+++ MaveSDK/Controllers/MAVESharer.m
@@ -45,14 +45,7 @@ NSString * const MAVESharePageShareTypeClipboard = @"clipboard";
     ownInstance.completionBlockClientSMS = completionBlock;
 
     MFMessageComposeViewController *composeVC = [MAVESharerViewControllerBuilder MFMessageComposeViewController];
-    MAVEUserData *maveUser = [MaveSDK sharedInstance].userData;
-    NSString *message;
-    if ( maveUser.inviteLinkDestinationURL && !maveUser.wrapInviteLink ) {
-        // If the inviteLinkDestinationURL is set, and the link should NOT be wrapped, pass the raw inviteLinkDestinationURL to the SMS VC
-        message = [[ownInstance.remoteConfiguration.clientSMS.text stringByAppendingString:@" "] stringByAppendingString:maveUser.inviteLinkDestinationURL];
-    } else {
-        message = [self shareCopyFromCopy:ownInstance.remoteConfiguration.clientSMS.text andLinkWithSubRouteLetter:@"s"];
-    }
+    NSString *message = [self shareCopyFromCopy:ownInstance.remoteConfiguration.clientSMS.text andLinkWithSubRouteLetter:@"s"];
 
     composeVC.messageComposeDelegate = ownInstance;
     composeVC.body = message;
